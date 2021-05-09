e515dd21c83eafde62c6a7e2033903abfbae2298
Reut-Maslansky <maslanskyr@gmail.com>
2021-05-09 11:35:55 +0300

    Using Player function

--- sources/Dispatcher.cpp
+++ sources/Dispatcher.cpp
@@ -4,17 +4,13 @@ using namespace std;
 
 namespace pandemic
 {
-    Dispatcher::Dispatcher(Board& b, City c) : Player(b, c) {}
+    Dispatcher::Dispatcher(Board &b, City c) : Player(b, c) {}
 
     string Dispatcher::role() { return "Dispatcher"; }
 
     Dispatcher &Dispatcher::fly_direct(City c)
     {
-         if(myLocation==c){
-            throw invalid_argument("Can't fly: This city is current location of the player");
-
-        }
-        if (myBoard.hasStation(myLocation))
+        if (myBoard.hasStation(myLocation) && myLocation != c)
         {
             myLocation = c;
         }
